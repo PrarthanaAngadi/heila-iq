31faf277434341e8f456380b3b9e9f455beb58d2
vineet-ld <vineet_ld@live.com>
2016-08-13 19:11:04 -0400

    Modified menu to display current page. Added about.html page.

--- Code/web/js/components/mainMenu.js
+++ Code/web/js/components/mainMenu.js
@@ -54,6 +54,11 @@ var Menu = React.createClass({
 		        </div>
 		    </nav>
 		);
+	},
+
+	componentDidMount: function(){
+		var page = $("#page-info").val();
+		$("ul.nav li a:eq(" + page + ")").addClass("selected");
 	}
 });
 
@@ -65,5 +70,5 @@ var MenuItem = React.createClass({
 	}
 })
 
-ReactDOM.render(<Menu items="Home:index.html, About:#, Media:#, Contact Us:# "/>, $("body > header")[0]);
+ReactDOM.render(<Menu items="Home:index.html, About:about.html, Media:#, Contact Us:# "/>, $("body > header")[0]);
 
