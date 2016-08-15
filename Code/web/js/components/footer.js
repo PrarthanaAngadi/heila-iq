1d822c4b43f16578527513e191ef1d2f8615efb7
vineet-ld <vineet_ld@live.com>
2016-08-15 16:54:34 -0400

    Modified Footer react component to added different content on home & about us page

--- Code/web/js/components/footer.js
+++ Code/web/js/components/footer.js
@@ -3,19 +3,67 @@
  */
 
 var Footer = React.createClass({
+
+    getInitialState: function(){
+        return {
+            con: <div>test</div>
+        }
+    },
+
+    getDefaultProps: function(){
+        return {
+            content: <div>test</div>
+        }
+    },
+
+    componentWillMount: function(){
+        var page = $("#page-info").val();
+        switch(page) {
+
+            /* Footer content for Home Page */
+            case "0":
+                this.props.content = (
+                    <div className="col-md-8 col-sm-12">
+                        <header>
+                            <h3>About Us</h3>
+                        </header>
+                        <section>
+                            <p>The Heila IQ was designed by MIT students and alumni with expertise on microgrids, control, computer science, and machine learning to make building and managing microgrids easier.</p>
+                        </section>
+                    </div>
+                );
+                break;
+
+            /* Footer content for About Page */
+            case "1":
+                this.props.content = (
+                    <div className="col-md-8 col-sm-12">
+                        <header>
+                            <h3>Contact Us</h3>
+                        </header>
+                        <section>
+                            <p>If you are interested in joining the team, please <a href="#">reach out to us</a>. We are particularly interested in people with backgrounds in control algorithms, systems integration, data analytics / machine learning.</p>
+                        </section>
+                    </div>
+                );
+                break;
+
+            /* Footer content for Media Page */
+            case "2":
+                break;
+
+            /* Footer content for Contact Us Page */
+            case "3":
+                break;
+        }
+    },
+
     render: function() {
         return (
             <div>
                 <div className="container">
                     <div className="row">
-                        <div className="col-md-8 col-sm-12">
-                            <header>
-                                <h3>About Us</h3>
-                            </header>
-                            <section>
-                                <p>The Heila IQ was designed by MIT students and alumni with expertise on microgrids, control, computer science, and machine learning to make building and managing microgrids easier.</p>
-                            </section>
-                        </div>
+                        {this.props.content}
                         <div className="col-md-4 col-sm-12">
                             <div className="row">
                                 <div className="col-md-2 col-md-offset-4 col-sm-3 col-sm-offset-3 col-xs-6 social-icons">
