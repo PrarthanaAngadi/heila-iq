910283993b9fbd3fd79285f7182b4168acd090d8
juilee <jadhav.j@husky.neu.edu>
2016-08-17 19:08:15 -0400

    Dashboard- Version 1.1

--- Code/web/js/controller/dashboardController.js
+++ Code/web/js/controller/dashboardController.js
@@ -1,3 +1,14 @@
 angular.module('userapp',[]).controller("UserController",['$scope',function ($scope) {
-
+    $scope.data = {
+        "entries": [{
+            "id": 2081,
+            "name": "BM",
+            "niceName": "bodmas"
+        }, {
+            "id": 8029,
+            "name": "Mas",
+            "niceName": "Masm"
+        }],
+        "count": 2
+    };
 }])
