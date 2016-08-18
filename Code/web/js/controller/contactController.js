ae27be979c4bb98f0964246c3ce36a5b17b6a6a3
juilee <jadhav.j@husky.neu.edu>
2016-08-17 20:58:41 -0400

    Added Angular http call

--- Code/web/js/controller/contactController.js
+++ Code/web/js/controller/contactController.js
@@ -2,19 +2,6 @@ var contactApp = angular.module('contactApp', []);
 
 contactApp.controller('contactController', function ($scope, $http) {
 
-    /*$scope.submitForm = function () {
-
-     $http({
-     method: 'POST',
-     url: 'abc.rb',
-     data: $scope.user,
-     headers: {'Content-Type': 'application/x-www-form-urlencoded'}
-     })
-     .success(function (data) {
-
-     })
-     }*/
-
     $scope.user = {
         name: "",
         company: "",
@@ -24,7 +11,37 @@ contactApp.controller('contactController', function ($scope, $http) {
     }
 
     $scope.sendEmail = function(){
+        var data = $('#contactForm').serializeObject();
+        data = JSON.stringify(data);
+        var url = 'http://localhost:3000/contact/email.json';
+        var config = {
+            headers : {
+                'Content-Type': 'application/json;'
+            }
+        }
 
+        $http.post(url, data, config)
+            .then(
+                function(response){
+                    alert("Success");
+                },
+                function(response){
+                    alert("Error");
+                }
+            );
+
+
+        /*$http({
+            method: 'POST',
+            url: 'http://localhost:3000/contact/email.json',
+            data: JSON.stringify(data)
+        }).then(function successCallback(response) {
+            alert('Success: '+response);
+        }, function errorCallback(response) {
+            alert('error: '+response);
+        });*/
     }
+
+
 
 });
