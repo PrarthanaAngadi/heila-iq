8e9cd05a435d6e83e2a4d2416abfcd72d58ccc91
juilee <jadhav.j@husky.neu.edu>
2016-08-18 15:49:18 -0400

    Users angular JS

--- Code/web/js/controller/dashboardController.js
+++ Code/web/js/controller/dashboardController.js
@@ -1,14 +1,24 @@
-angular.module('userapp',[]).controller("UserController",['$scope',function ($scope) {
-    $scope.data = {
-        "entries": [{
-            "id": 2081,
-            "name": "BM",
-            "niceName": "bodmas"
-        }, {
-            "id": 8029,
-            "name": "Mas",
-            "niceName": "Masm"
-        }],
-        "count": 2
-    };
-}])
+angular.module('userapp',[]).controller("UserController",['$scope',function ($scope,$http) {
+    $scope.getUsers = function() {
+        /*var data = $('#userForm').serializeObject();
+        data = JSON.stringify(data);
+        alert(data);*/
+        var url = 'http://localhost:3000/users.json';
+        var config = {
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
+            }
+        }
+
+        $http.get(url, config)
+            .then(
+                function (response) {
+                  $scope.users= response.data;
+                },
+                function (response) {
+                    alert("Error");
+                }
+            );
+    }
+
+    }])
