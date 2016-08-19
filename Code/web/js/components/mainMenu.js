1ef3c862676207f1a4c4ed1ca31723b5f62216ef
juilee <jadhav.j@husky.neu.edu>
2016-08-19 05:21:24 -0400

    Merged all conflicts after test service merge

--- Code/web/js/components/mainMenu.js
+++ Code/web/js/components/mainMenu.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 var Menu = React.createClass({
 
     getInitialState: function () {
@@ -75,81 +74,4 @@ var MenuItem = React.createClass({
 ReactDOM.render(<Menu
     items="Home:index.html, About:about.html, Media:#, Contact Us:contact.html "/>, $("body > header")[0]);
 
-=======
-var Menu = React.createClass({
 
-    getInitialState: function () {
-        return {
-            linkItems: []
-        }
-    },
-
-    componentWillMount: function () {
-        if (this.props.items) {
-            var items = this.props.items.split(",");
-            var anchorList = [];
-            $.each(items, function (i, elem) {
-                var anchorElem = elem.trim().split(":");
-                var obj = {"title": anchorElem[0], "link": anchorElem[1]};
-                anchorList.push(obj);
-            })
-            this.setState({linkItems: anchorList});
-        }
-    },
-
-    render: function () {
-        return (
-            <nav className="navbar navbar-default navbar-fixed-top" role="navigation">
-                <div className="container">
-                    <div className="navbar-header">
-                        {
-                            this.state.linkItems.map(function (item, i) {
-                                if (i == 0) {
-                                    return (
-                                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse"
-                                                data-target="#main-menu" aria-expanded="false">
-                                            <span className="sr-only">Toggle navigation</span>
-                                            <span className="icon-bar"></span>
-                                            <span className="icon-bar"></span>
-                                            <span className="icon-bar"></span>
-                                        </button>
-                                    );
-                                }
-                            })
-                        }
-                        <a className="navbar-brand" href={"index.html"}><img src={"images/logo.png"}/></a>
-                    </div>
-                    <div className="collapse navbar-collapse" id="main-menu">
-                        <ul className="nav navbar-nav navbar-right">
-                            {
-                                this.state.linkItems.map(function (item, i) {
-                                    return (
-                                        <MenuItem link={item.link} title={item.title}/>
-                                    );
-                                })
-                            }
-                        </ul>
-                    </div>
-                </div>
-            </nav>
-        );
-    },
-
-    componentDidMount: function () {
-        var page = $("#page-info").val();
-        $("ul.nav li a:eq(" + page + ")").addClass("selected");
-    }
-});
-
-var MenuItem = React.createClass({
-    render: function () {
-        return (
-            <li><a href={this.props.link}>{this.props.title}</a></li>
-        );
-    }
-})
-
-ReactDOM.render(<Menu
-    items="Home:index.html, About:about.html, Media:#, Contact Us:contact.html "/>, $("body > header")[0]);
-
->>>>>>> Web-Service-Test
--- Code/web/js/controller/dashboardController.js
+++ Code/web/js/controller/dashboardController.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 angular.module('userapp',[]).controller("UserController",['$scope',function ($scope,$http) {
     $scope.getUsers = function() {
         /*var data = $('#userForm').serializeObject();
@@ -23,48 +22,4 @@ angular.module('userapp',[]).controller("UserController",['$scope',function ($sc
     }
 
     }])
-=======
-angular.module('userapp',[]).controller("UserController",['$scope',function ($scope,$http) {
-    $scope.getUsers = function() {
-        var url = 'http://localhost:3000/users.json';
-        var config = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-            }
-        }
 
-        $http.get(url, config)
-            .then(
-                function (response) {
-                  $scope.users= response.data;
-                },
-                function (response) {
-                    alert("Error");
-                }
-            );
-    }
-    $scope.addUser = function()
-    {
-        var data = $('#userForm').serializeObject();
-         data = JSON.stringify(data);
-         alert(data);
-        var url = 'http://localhost:3000/users.json';
-        var config = {
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded;charset=utf-8;'
-            }
-        }
-
-        $http.post(url,data, config)
-            .then(
-                function (response) {
-                   alert("Success")
-                },
-                function (response) {
-                    alert("Error");
-                }
-            );
-    }
-
-    }])
->>>>>>> Web-Service-Test
