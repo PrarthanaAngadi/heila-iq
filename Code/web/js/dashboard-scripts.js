275732d1f12c2e239d11609fa300dc60fb671142
PrarthanaAngadi <prarthana.angadi@rentalbeast.com>
2016-08-19 03:38:22 -0400

    Combined web & service

---
+++ Code/web/js/dashboard-scripts.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+    $( "#dob" ).datepicker();
+  /*  $('.modal').on('hidden.bs.modal', function (e) {
+        $('form')[0].reset();
+        $("form input").attr('class', '');
+        $('form p').attr('class','');
+
+    })
+    $('.modal').on('show.bs.modal', function (e) {
+        $('form input').attr('class','form-control ng-pristine ng-untouched ng-empty ng-invalid ng-invalid-required ng-valid-pattern');
+        $('form p').attr('class','error ng-hide');
+    })*/
+
+});
